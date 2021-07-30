c538d578214ccf18f448d1f03f4a8125d0f97b07
Pushpinder <pushpinder.kaur@revature.com>
2021-07-30 14:44:24 -0400

    until, select and for loop

--- 01Scripting/Notes.md
+++ 01Scripting/Notes.md
@@ -128,3 +128,12 @@ do
   command(s) 
 done
 ```
+- `select` Constructs simple menu from word list. It Allows user to enter a number instead of a word. So User enters sequence number corresponding to the word.
+Syntax:
+```
+select WORD in LIST     
+do           
+	statements
+done 
+```
+Loops until end of input, i.e. ^d  (or ^c)
--- 01Scripting/Shell Scripting/Loopings.sh
+++ 01Scripting/Shell Scripting/Loopings.sh
@@ -21,14 +21,77 @@
 #     statement(s)
 # done
 
-n=1
-#while [ $n -le 10 ]
-while  (($n <= 10)) 
+# n=1
+# #while [ $n -le 10 ]
+# while  (($n <= 10)) 
+# do
+# if [ $(( n%2 )) -eq 0 ]
+# then
+#    echo "$n"
+# fi
+#    #n=$(( n+1 ))
+#    (( n++ ))
+# done
+
+# Until loop
+# n=1
+# #until [ $n -gt 10 ]
+# until (( $n > 10 ))
+# do
+#     echo $n
+#     (( n++ ))
+# done
+
+# FOR Loop
+#for variable in unix-or-linux-commands
+# for (( exp1; exp2; exp3 ))
+# do  
+#     statement(s)
+# done
+
+#for temp in 1 2 3 4 5
+# for temp in {1..10..2} #seed, range, increment number
+# do
+#     echo $temp
+# done
+
+# for (( i=0; i<6; i++ ))
+# do 
+#     echo $i
+# done
+
+# for command in date pwd ls $BASH_VERSION $HOME
+# do 
+#     echo "---------------------$command--------------------"
+#     $command
+# done
+
+# Select loop
+# select variable in list
+# do
+#     statement(s)
+# done
+
+select lang in C C# Java Python Ruby
 do
-if [ $(( n%2 )) -eq 0 ]
-then
-   echo "$n"
-fi
-   #n=$(( n+1 ))
-   (( n++ ))
+    #echo "$lang has been selected"
+    case $lang in
+        C)
+            echo C programming language has been selected
+            ;;
+        C#)
+            echo C# programming language has been selected
+            ;;
+        Java)
+            echo Java programming language has been selected
+            ;;
+        Python)
+            echo Python programming language has been selected
+            ;;
+        Ruby)
+            echo Ruby programming language has been selected
+            ;;
+        *)
+            echo " Error! Please selected choices between 1 to 5"
+    esac
 done
