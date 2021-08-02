dedf8163d9819efde74d884df324d4043279950f
Juniper Song <songminseonk@gmail.com>
2021-08-02 15:01:52 -0400

    basic i/o

--- 02DotNet/HelloWorld/Program.cs
+++ 02DotNet/HelloWorld/Program.cs
@@ -7,6 +7,18 @@ namespace HelloWorld
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Revature!");
+            Console.WriteLine("The current time is " + DateTime.Now);
+
+            Console.WriteLine("Enter something: ");
+            string input = Console.ReadLine();
+
+            if(input == "") {
+                Console.WriteLine("You entered nothing");
+            }
+            else {
+                Console.WriteLine("You Entered " + input);
+            }
         }
     }
 }
