81c36ae242c7ed392853537ddc930b841ffa93c5
Pushpinder <pushpinder.kaur@revature.com>
2021-08-11 15:10:30 -0400

    Polymorphism

--- 03CSharp/PetManagment/Lib/Cat.cs
+++ 03CSharp/PetManagment/Lib/Cat.cs
@@ -28,8 +28,29 @@ namespace Lib
         public FurType FurType { get; set; }
 
         public new string GetDetails(){
-            var result=base.GetDetails();
+            var result=base.GetDetails();// calling parent class Getdetails method
             return result +" " + $"\nCat type: {CatType}\nWeight: {Weight} pounds\nFur type: {FurType}";
+        }
+        public override string GetMeal(Gender Gender)
+        {
+            Meal meal=new Meal(DateTime.Now);
+            if(Gender == Gender.Male){
+                if(TimeConversion.GetTime(meal.Time.ToLongDateString())<12&&TimeConversion.GetTime(meal.Time.ToLongDateString())>5)
+                    return $"125 gms of {FoodType.DryFood}";
+                else if (TimeConversion.GetTime(meal.Time.ToLongDateString())>=12 && TimeConversion.GetTime(meal.Time.ToLongDateString())<=18)
+                     return $"150 gms of {FoodType.WetFood}";
+                else 
+                    return $"150 gms of {FoodType.RawFood}";
+               }
+            else
+              {
+                if(TimeConversion.GetTime(meal.Time.ToLongDateString())<12 && TimeConversion.GetTime(meal.Time.ToLongDateString())>5)
+                    return $"100 gms of {FoodType.DryFood}";
+                else if (TimeConversion.GetTime(meal.Time.ToLongDateString())>=12 && TimeConversion.GetTime(meal.Time.ToLongDateString())<=18)
+                     return $"120 gms of {FoodType.WetFood}";
+                else 
+                    return $"100 gms of {FoodType.RawFood}";
+              }
         }
 
     }
