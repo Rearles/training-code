ad47aa2ecc4eacb6c51c34e74a06ec440a5a7218
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-08-23 13:36:29 -0400

    add some comments and logging

--- 04SQL/Models/Meal.cs
+++ 04SQL/Models/Meal.cs
@@ -1,19 +1,23 @@
-using System;
+﻿using System;
 
 namespace Models
 {
     public class Meal
     {
-        public Meal() {}
+        public Meal() { }
         public Meal(int catId, string foodType)
         {
             this.Time = DateTime.Now;
             this.CatId = catId;
+            if (string.IsNullOrWhiteSpace(foodType))
+            {
+                throw new ArgumentException("invalid value for food type");
+            }
             this.FoodType = foodType;
         }
 
-        public DateTime Time {get;set;}
-        public string FoodType {get;set;}
-        public int CatId {get;set;}
+        public DateTime Time { get; set; }
+        public string FoodType { get; }
+        public int CatId { get; set; }
     }
 }
--- 04SQL/UI/Program.cs
+++ 04SQL/UI/Program.cs
@@ -5,6 +5,12 @@ using DL.Entities;
 using BL;
 using DL;
 using System.IO;
+using Serilog;
+using System;
+
+// be careful making assumptions about Directory.GetCurrentDirectory() --
+// visual studio and dotnet run make that the project source code directory,
+// but that's only how devs run .net apps, not how they get run for real users.
 
 var configuration = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
@@ -22,5 +28,22 @@ var context = new petdbContext(options);
 // HealthStatus status;
 // var obj=status.getInstance;
 
-IMenu menu = new MainMenu(new PetBL(new PetRepo(context)));
-menu.Start();
+try
+{
+
+    IMenu menu = new MainMenu(new PetBL(new PetRepo(context)));
+    menu.Start();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "program must exit");
+}
+
+
+// ON DELETE CASCADE on a foreign key makes it so:
+// DELETE FROM Cat WHERE Id = 5
+//   rather than an error...
+// "1 row affected" (in cat table)
+// "3000 rows affected" (all the meals that referenced it)
+// sometimes this is what you want, other times not.
+// decision to be made for each foreign key relationship constraint.
