4f47fb8acc2e9f1f212246b6213b4d58c031b897
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-01 17:10:33 -0400

    add unit tests

---
+++ 05MVC/NoteTakingApp/NoteTakingApp.UnitTests/Domain/NoteServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NoteTakingApp.Domain;
+using Xunit;
+
+namespace NoteTakingApp.UnitTests.Domain
+{
+    public class NoteServiceTests
+    {
+        [Fact]
+        public void RemoveDuplicates_ReturnsZero()
+        {
+            // arrange
+            var service = new NoteService(repo: null);
+
+            // act
+            var result = service.RemoveDuplicateNotes();
+
+            // assert
+            Assert.Equal(expected: 0, actual: result);
+        }
+    }
+}
