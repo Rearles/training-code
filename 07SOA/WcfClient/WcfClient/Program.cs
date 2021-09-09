6161c5be548356c73993bfa8c60f178f2c19f37c
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-09 11:31:33 -0400

    generate wcf client

---
+++ 07SOA/WcfClient/WcfClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WcfClient.DataService;
+
+namespace WcfClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var client = new Service1Client())
+            {
+                Console.WriteLine(client.GetData(4));
+                Console.WriteLine(client.GetDataUsingDataContract(new CompositeType
+                {
+                    BoolValue = true,
+                    StringValue = "asdf"
+                }).StringValue);
+            }
+            Console.ReadKey();
+        }
+    }
+}
