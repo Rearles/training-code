6f4c9b7d735cc9767a97e933ea8efacd8a0f3527
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-09 16:35:51 -0400

    add quick web api demo

---
+++ 07SOA/HelloRest/HelloRest/Controllers/EmailController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HelloRest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace HelloRest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        // GET: api/Email
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/Email/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/Email
+        [HttpPost]
+        public void Post([FromBody] Email email)
+        {
+
+        }
+
+        // PUT api/Email/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Email email)
+        {
+        }
+        
+        // DELETE api/Email/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- 07SOA/HelloRest/HelloRest/Controllers/WeatherForecastController.cs
+++ 07SOA/HelloRest/HelloRest/Controllers/WeatherForecastController.cs
@@ -24,16 +24,16 @@ namespace HelloRest.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IActionResult Get()
         {
             var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Ok(Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToArray());
         }
     }
 }
