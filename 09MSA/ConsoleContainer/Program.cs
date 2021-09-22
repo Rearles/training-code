65dc2b8db97fd371b28acc9db16711f3ec93e86d
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-22 11:25:20 -0400

    dockerize console app (run)

--- 09MSA/ConsoleContainer/Program.cs
+++ 09MSA/ConsoleContainer/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleContainer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World v2!");
         }
     }
 }
