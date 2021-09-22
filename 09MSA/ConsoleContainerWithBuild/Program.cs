63b775ac70373a677467799a47a913b7177f20d0
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-09-22 12:02:46 -0400

    generate new console app

---
+++ 09MSA/ConsoleContainerWithBuild/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleContainerWithBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
